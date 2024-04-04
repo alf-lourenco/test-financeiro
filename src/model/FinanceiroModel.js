f1b375c7e552a4e6ba07cdbffb176f9a326add72
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-04-04 12:12:29 -0300

    criacao do model de despesas

--- src/model/FinanceiroModel.js
+++ src/model/FinanceiroModel.js
@@ -1,8 +1,40 @@
-const { Model } = require('mongoose');
+const mongoose = require('mongoose');
 
-const depesa = new Model.schema({
+const despesa = new mongoose.Schema({
   nomeItem: {
     type: String,
     required: true,
   },
+  valor: {
+    type: Number,
+    required: true,
+  },
+
+  vencimento: {
+    type: Date,
+    required: true,
+  },
+  quantidadeParcelas: {
+    type: Number,
+    required: true,
+  },
+  numeroParcela: {
+    type: Number,
+    required: true,
+  },
+  responsavel: {
+    type: String,
+    default: 'Alfredo',
+  },
+  situacaoPagamento: {
+    type: String,
+    default: 'Pendente',
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
 });
+
+const Despesa = mongoose.model('Gastos', despesa);
+module.exports = Despesa;
